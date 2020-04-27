68ae003946de35599b22f53d68c3def442f51437
sibbte <34045901+sibbte@users.noreply.github.com>
2020-04-27 21:33:37 +0100

    added class diagram

---
+++ Migrations/20200427120244_Customer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CustomerMicroservice.Migrations
+{
+    public partial class Customer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
